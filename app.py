36760f9ed9ba249ceafb2c09d852fbd4420b4a30
Svetlana <selimargo1986@gmail.com>
2026-03-30 17:07:26 +0300

    project

--- app.py
+++ app.py
@@ -161,7 +161,8 @@ def export_excel():
 # ------------------- Запуск сервера -------------------
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+
+    if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 7860))
+        app.run(host="0.0.0.0", port=port)
 
